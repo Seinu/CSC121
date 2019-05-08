bf1cb007e09a787fd73a10271a5e5e136844f644
Zen <zen@Zens-MacBook-Pro.local>
2019-05-07 20:31:09 -0400

    Updated Final for public view

--- Final/customer.py
+++ Final/customer.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2019 Seinu
+#
+# This file is part of CSC121.
+# NOT FOR COLLEGE REUSE
+#
+# CSC121 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CSC121 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CSC121.  If not, see <http://www.gnu.org/licenses/>.
+#
+# NOT FOR COLLEGE REUSE
+
 import re
 
 
@@ -13,12 +33,6 @@ class Customer:
         self._security_code = ""
 
     def input_age(self):
-        """
-        In this method, the user enters age.  Age is invalid if it is negative or if the input string cannot 
-        be converted to an integer.  If an invalid age is entered, display an error message and ask the user 
-        to reenter.  Repeat this until the age is valid.  Store the valid age in the instance variable age. 
-        This method has no parameter (except self) and no return value.
-        """
         while(self._age < 1):
             try:
                 self._age = int(input("Please enter age: "))
@@ -28,13 +42,6 @@ class Customer:
                 print("Error age can't be negative!")
 
     def input_password(self):
-        """
-        In this method, the user enters a password meeting the password requirements.  The password must be 8-12 
-        characters in length, and must contain at least one upper-case letter, one lower-case letter, and one 
-        number.  If an invalid password is entered, display an error message and ask the user to reenter.  
-        Repeat this until the password is valid.  Store the valid password in the instance variable password. 
-        This method has no parameter (except self) and no return value.
-        """
         correct = False
 
         while(correct == False):
@@ -52,13 +59,6 @@ class Customer:
                 correct = True
 
     def input_card_number(self):
-        """
-        In this method, the user enters a 16-digit credit card number.  The card number is invalid if the length 
-        is not 16 or if it has one or more non-digit characters.  If an invalid card number is entered, display 
-        an error message and ask the user to reenter.  Repeat this until the card number is valid.  Store the 
-        valid card number in the instance variable card_number. This method has no parameter (except self) and 
-        no return value.
-        """
         correct = False
         while(correct == False):
             self._card_number = input(
@@ -71,13 +71,6 @@ class Customer:
                 correct = True
 
     def input_security_code(self):
-        """
-        In this method, the user enters a 3-digit credit card security code.  The security code is invalid if the 
-        length is not 3 or if it has one or more non-digit characters.  If an invalid security code is entered, 
-        display an error message and ask the user to reenter.  Repeat this until the security code is valid.  
-        Store the valid security code in the instance variable security_code. This method has no parameter 
-        (except self) and no return value.
-        """
         correct = False
         while(correct == False):
             self._security_code = input(
@@ -90,12 +83,6 @@ class Customer:
                 correct = True
 
     def input_info(self):
-        """
-        This method allows the user to enter first name, last name and other personal information.  First, ask 
-        the user to enter first name and last name and store them in the instance variables first_name and 
-        last_name.  Then call the following methods to enter other personal information: input_age, input_password, 
-        input_card_number and input_security_code. This method has no parameter (except self) and no return value.
-        """
         self._first_name = input("Please enter your first name.\n")
         self._last_name = input("Please enter your last name.\n")
         self.input_age()
@@ -104,12 +91,6 @@ class Customer:
         self.input_security_code()
 
     def verify_info(self):
-        """
-        This method allows the user to update any item previously entered before completing the registration. 
-        First, display all instance variables.  Then ask the user to choose one item to update.  Write code or 
-        call appropriate method to update the chosen item.  Repeat this until the user does not want to make 
-        any more changes. This method has no parameter (except self) and no return value.
-        """
         verify = False
         while(verify == False):
             print("Email: ", self._email, "\n",
@@ -145,11 +126,4 @@ class Customer:
                     self._email = input("Please enter a email: ")
 
     def output_info(self):
-        """
-        This method creates and returns a string that consists of all personal information of the customer.  
-        Data in the string must follow this order: first_name, last_name, age, email, password, card_number 
-        and security_code. Insert a space after each item except the security_code.  Insert a newline 
-        character, i.e. ‘\n’, at the end of the string.  The returned string will be used later to write 
-        customer’s personal information to a textfile. This method has no parameter (except self).
-        """
         return "%s %s %s %s %s %s %s\n" % (self._first_name, self._last_name, self._age, self._email, self._password, self._card_number, self._security_code)
